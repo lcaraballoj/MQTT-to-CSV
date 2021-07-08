c72589661ecbce3a20fe333ad243e556adfa5595
Linnea Caraballo <ljc42000@gmail.com>
2021-07-07 21:38:41 -0400

    Trying to get MQTT message to csv.

--- MqttSubscribe.py
+++ MqttSubscribe.py
@@ -1,5 +1,10 @@
 import paho mqtt.client as mqtt
 import time
+import json
+import random
+import datetime
+import csv
+import pandas as pd
 
 def on_message (client, userdata, message):
         print ("Received message: ", str(message.payload.decode('utf-8')))
@@ -14,6 +19,17 @@ client.loop_start()
 
 client.subscribe(topic = "Test", qos = 1)
 client.on_message = on_message
+
+msg = eval(client.on_message)
+
+fileName = str(currentDateTime.strftime("%Y%m%dT%H%M%S"))+'.csv'
+
+df = pd.DataFrame.from_dict(msg)
+
+df.to_csv(dateTime, index = False, header=True)
+
+print ("Recorded in CSV file")
+
 time.sleep(300)
 
 client.loop_end()
